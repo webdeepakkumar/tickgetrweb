ebec48b92fb534bbce93fc1ae3f0567cd3a5d8ef
Tumhara Naam <tumhara@email.com>
2025-08-07 15:36:35 +0530

    Feat : Add Privacy Page Title And Description (EN Version)

--- app/[locale]/(root)/(static)/(policies)/privacy/page.jsx
+++ app/[locale]/(root)/(static)/(policies)/privacy/page.jsx
@@ -1,21 +1,29 @@
-import React from "react";
-import Policy from "@/app/[locale]/components/Policy";
-import { useTranslations } from "next-intl";
+import dynamic from "next/dynamic";
 import { unstable_setRequestLocale } from "next-intl/server";
 
-const PrivacyPage = ({ params: { locale } }) => {
+// Dynamically import the actual page component
+const PrivacyPage = dynamic(() => import("./PrivacyPage"), { ssr: false });
+
+export const generateMetadata = ({ params }) => {
+  const locale = params.locale;
+
+  const titles = {
+    en: "Privacy Policy | Tickgetr",
+    nl: "Privacybeleid | Tickgetr",
+  };
+
+  const descriptions = {
+    en: "Learn how TickGetr protects your data with our Privacy Policy. Clear details on information collection, usage, and security measures for users and organizers.",
+    nl: "Ontdek hoe TiGer uw gegevens beschermt met ons privacybeleid. Duidelijke informatie over het verzamelen, gebruiken en de beveiligingsmaatregelen voor gebruikers en organisatoren.",
+  };
+
+  return {
+    title: titles[locale] || titles.en,
+    description: descriptions[locale] || descriptions.en,
+  };
+}; 
+
+export default function Page({ params: { locale } }) {
   unstable_setRequestLocale(locale);
-
-  const t = useTranslations("privacyData");
-
-  const title = t.raw("title");
-  const policies = t.raw("policies");
-
-  return (
-    <div>
-      <Policy title={title} policies={policies} />
-    </div>
-  );
-};
-
-export default PrivacyPage;
+  return <PrivacyPage />;
+}
