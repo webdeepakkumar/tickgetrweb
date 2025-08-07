98881760a52b88a35dd36bc0b2c02297a3b87c05
Tumhara Naam <tumhara@email.com>
2025-08-07 12:36:12 +0530

    Feat : add Contact page title and description (EN Version)

--- app/[locale]/(root)/(static)/contact/page.jsx
+++ app/[locale]/(root)/(static)/contact/page.jsx
@@ -13,6 +13,36 @@ import {
 import { FaAngleDown } from "react-icons/fa6";
 import { useTranslations } from "next-intl";
 
+import dynamic from "next/dynamic";
+import { unstable_setRequestLocale } from "next-intl/server";
+
+// Load your client component
+const ContactPage = dynamic(() => import("./ContactPage"), { ssr: false });
+
+export const generateMetadata = ({ params }) => {
+  const locale = params.locale;
+
+  const titles = {
+    en: "Contact Us | Tickgetr",
+    nl: "Neem Contact Op | Tickgetr",
+  };
+
+  const descriptions = {
+    en: "Get in touch with TickGetr support or sales team. Have questions about event ticketing? Contact us for fast, friendly help and support.",
+    nl: "Neem contact op met het TickGetr-team voor ondersteuning of verkoop. Vragen over online ticketverkoop? Wij helpen je graag verder.",
+  };
+
+  return {
+    title: titles[locale] || titles.en,
+    description: descriptions[locale] || descriptions.en,
+  };
+};
+
+export default function Page({ params: { locale } }) {
+  unstable_setRequestLocale(locale);
+  return <ContactPage />;
+}
+
 const Contact = () => {
   const t = useTranslations("contact");
 
